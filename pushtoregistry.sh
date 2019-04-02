d8dceaf3019dc301e6bccd7c49cf79ae82207cca
Laurent Corbes <caf@glot.net>
2019-04-02 16:03:33 +0200

    Fix verbose script exec

--- pushtoregistry.sh
+++ pushtoregistry.sh
@@ -1,7 +1,11 @@
 #!/bin/env bash
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}"
-[[ $distribution_alias ]] && \
-  docker tag "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}" "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_alias}" \
+set -v -x
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &&
+docker push "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}" &&
+[[ $distribution_alias ]] &&
+  {
+  docker tag "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}" "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_alias}" \ &&
   docker push "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_alias}"
+}
