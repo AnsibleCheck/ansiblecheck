afb51664cde8861fc667cb7384a7e7e85103e597
Laurent Corbes <caf@glot.net>
2019-04-02 18:11:16 +0200

    Another Fix push script

--- pushtoregistry.sh
+++ pushtoregistry.sh
@@ -4,9 +4,9 @@ set -v -x
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &&
 docker push "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}" &&
-[[ $distribution_alias != '' ]] &&
+[[ "x${distribution_alias}" != "x" ]] &&
   {
   docker tag "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_version}" "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_alias}" &&
   docker push "${DOCKER_IMAGE_SLUG}":"${distribution,,}"-"${distribution_alias}"
-} &&
+} ||
 : || exit 2
